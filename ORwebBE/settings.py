ce236ff9c34cf0c8ae8a99306fbfe4d4094e1bda
willsenyogi <willsenyp21@gmail.com>
2025-10-15 16:45:45 +0700

    CORS configuration

--- ORwebBE/settings.py
+++ ORwebBE/settings.py
@@ -59,6 +59,27 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS'
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 ROOT_URLCONF = 'ORwebBE.urls'
 
 TEMPLATES = [
