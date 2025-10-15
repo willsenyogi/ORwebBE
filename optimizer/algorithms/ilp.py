2a88500f5758e6eddb3ab712136801c76fa49e35
willsenyogi <willsenyp21@gmail.com>
2025-10-15 17:03:00 +0700

    post api draft

--- optimizer/algorithms/ilp.py
+++ optimizer/algorithms/ilp.py
@@ -13,50 +13,51 @@ class SolverILP:
         self.total_vehicles = sum(self.vehicles_per_depot)
         self.vehicle_depot_map = [idx for idx, num in enumerate(self.vehicles_per_depot) for _ in range(num)]
 
-    def run(self, verbose=False):
-        start = time.time()
-        all_points = self.depots + self.customers; num_points = len(all_points)
-        dist_matrix = DistanceMatrix.build_distance_matrix(all_points)
-        full_demands = [0] * self.m + self.customer_demands
-        depot_indices, customer_indices, vehicle_indices = list(range(self.m)), list(range(self.m, num_points)), list(range(self.total_vehicles))
-        model = gp.Model("MDVRP_ILP_Dynamic_Capacity")
-        if not verbose: model.setParam('OutputFlag', 0)
-        x = model.addVars(num_points, num_points, self.total_vehicles, vtype=GRB.BINARY, name="x")
-        u = model.addVars(num_points, self.total_vehicles, vtype=GRB.CONTINUOUS, name="u")
-        model.setObjective(gp.quicksum(dist_matrix[i, j] * x[i, j, k] for i in range(num_points) for j in range(num_points) for k in vehicle_indices if i != j), GRB.MINIMIZE)
-        for j in customer_indices: model.addConstr(gp.quicksum(x[i, j, k] for i in range(num_points) for k in vehicle_indices if i != j) == 1)
-        for k in vehicle_indices: model.addConstr(gp.quicksum(x[self.vehicle_depot_map[k], j, k] for j in customer_indices) <= 1)
-        for k in vehicle_indices:
-            for j in customer_indices: model.addConstr(gp.quicksum(x[i, j, k] for i in range(num_points) if i != j) == gp.quicksum(x[j, i, k] for i in range(num_points) if i != j))
-        for k in vehicle_indices: model.addConstr(gp.quicksum(x[self.vehicle_depot_map[k], j, k] for j in customer_indices) == gp.quicksum(x[j, self.vehicle_depot_map[k], k] for j in customer_indices))
-        for k in vehicle_indices:
-            for i in range(num_points):
-                for j in customer_indices:
-                    if i != j: model.addConstr((x[i, j, k] == 1) >> (u[i, k] + full_demands[j] == u[j, k]))
-        for k in vehicle_indices:
-            for i in customer_indices:
-                model.addConstr(u[i, k] >= full_demands[i])
-                model.addConstr(u[i, k] <= self.vehicle_capacities[k])
-        
-        # model.setParam('NodefileStart', 0.7)
-        # model.setParam('TimeLimit', self.time_limit)
-        model.Params.NodefileStart = 0.5
-        model.Params.NodefileDir = "/tmp"
-        model.Params.Cuts = 1
-        model.Params.Presolve = 2
-        model.Params.Aggregate = 2
-        model.Params.PreCrush = 1
-        model.Params.MIPGap = 0.0
-        model.optimize()
-        end = time.time()
-        best_assignment = [-1] * self.n
-        best_value = model.ObjVal if model.Status in [GRB.OPTIMAL, GRB.TIME_LIMIT, GRB.SUBOPTIMAL] else float('inf')
-        if best_value < float('inf'):
-            solution = model.getAttr('x', x)
-            for i in range(num_points):
-                for j in customer_indices:
-                    for k in vehicle_indices:
-                        if solution[i, j, k] > 0.5:
-                            customer_idx_in_list = j - self.m
-                            best_assignment[customer_idx_in_list] = k
-        return {"best_assignment": best_assignment, "best_value": best_value, "time": end - start, "history": []}
+    def run(self, verbose=False, run_ilp=True):
+        if run_ilp == True:
+            start = time.time()
+            all_points = self.depots + self.customers; num_points = len(all_points)
+            dist_matrix = DistanceMatrix.build_distance_matrix(all_points)
+            full_demands = [0] * self.m + self.customer_demands
+            depot_indices, customer_indices, vehicle_indices = list(range(self.m)), list(range(self.m, num_points)), list(range(self.total_vehicles))
+            model = gp.Model("MDVRP_ILP_Dynamic_Capacity")
+            if not verbose: model.setParam('OutputFlag', 0)
+            x = model.addVars(num_points, num_points, self.total_vehicles, vtype=GRB.BINARY, name="x")
+            u = model.addVars(num_points, self.total_vehicles, vtype=GRB.CONTINUOUS, name="u")
+            model.setObjective(gp.quicksum(dist_matrix[i, j] * x[i, j, k] for i in range(num_points) for j in range(num_points) for k in vehicle_indices if i != j), GRB.MINIMIZE)
+            for j in customer_indices: model.addConstr(gp.quicksum(x[i, j, k] for i in range(num_points) for k in vehicle_indices if i != j) == 1)
+            for k in vehicle_indices: model.addConstr(gp.quicksum(x[self.vehicle_depot_map[k], j, k] for j in customer_indices) <= 1)
+            for k in vehicle_indices:
+                for j in customer_indices: model.addConstr(gp.quicksum(x[i, j, k] for i in range(num_points) if i != j) == gp.quicksum(x[j, i, k] for i in range(num_points) if i != j))
+            for k in vehicle_indices: model.addConstr(gp.quicksum(x[self.vehicle_depot_map[k], j, k] for j in customer_indices) == gp.quicksum(x[j, self.vehicle_depot_map[k], k] for j in customer_indices))
+            for k in vehicle_indices:
+                for i in range(num_points):
+                    for j in customer_indices:
+                        if i != j: model.addConstr((x[i, j, k] == 1) >> (u[i, k] + full_demands[j] == u[j, k]))
+            for k in vehicle_indices:
+                for i in customer_indices:
+                    model.addConstr(u[i, k] >= full_demands[i])
+                    model.addConstr(u[i, k] <= self.vehicle_capacities[k])
+            
+            # model.setParam('NodefileStart', 0.7)
+            # model.setParam('TimeLimit', self.time_limit)
+            model.Params.NodefileStart = 0.5
+            model.Params.NodefileDir = "/tmp"
+            model.Params.Cuts = 1
+            model.Params.Presolve = 2
+            model.Params.Aggregate = 2
+            model.Params.PreCrush = 1
+            model.Params.MIPGap = 0.0
+            model.optimize()
+            end = time.time()
+            best_assignment = [-1] * self.n
+            best_value = model.ObjVal if model.Status in [GRB.OPTIMAL, GRB.TIME_LIMIT, GRB.SUBOPTIMAL] else float('inf')
+            if best_value < float('inf'):
+                solution = model.getAttr('x', x)
+                for i in range(num_points):
+                    for j in customer_indices:
+                        for k in vehicle_indices:
+                            if solution[i, j, k] > 0.5:
+                                customer_idx_in_list = j - self.m
+                                best_assignment[customer_idx_in_list] = k
+            return {"best_assignment": best_assignment, "best_value": best_value, "time": end - start, "history": []}
--- optimizer/views.py
+++ optimizer/views.py
@@ -9,7 +9,31 @@ from .algorithms.ga import GeneticAlgorithm
 
 class OptimizeView(APIView):
     def post(self, request):
+        run_ilp = request.query_params.get('run_ilp', False)
         data = request.data
+        
+        depots = data['depots']
+        customers = data['customers']
+        vehicles_per_depot = data['vehicle_per_depot']
+        vehicle_capacities = data['vehichle_capacities']
+        customer_demands = data['customer_demands']
+        
+        if run_ilp == 'True':
+            run_ilp = True
+        else:
+            run_ilp = False
+            
+        
+        ilp=SolverILP(
+            depots=depots,
+            customers=customers,
+            vehicles_per_depot=vehicles_per_depot,
+            vehicle_capacities=vehicle_capacities,
+            customer_demands=customer_demands
+        )
+        
+        res_ilp=ilp.run(verbose=True, run_ilp=run_ilp)
+        
         return Response({"result": "result"}, status=200)
         
 
